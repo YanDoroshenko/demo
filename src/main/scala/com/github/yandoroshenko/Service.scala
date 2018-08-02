08792e25f0a7ac40787e2ddc0587a1c7084563ff
Yan Doroshenko <yandoroshenko@protonmail.com>
2018-08-02 18:31:30 +0200

    [new] Add a graceful shutdown

--- src/main/scala/com/github/yandoroshenko/Service.scala
+++ src/main/scala/com/github/yandoroshenko/Service.scala
@@ -5,12 +5,17 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.github.yandoroshenko.Routes._
 
+import scala.concurrent.ExecutionContextExecutor
+
 /**
   * Created by Yan Doroshenko (yandoroshenko@protonmail.com) on 02.08.2018.
   */
 object Service extends App {
   implicit val actorSystem: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
 
-  Http().bindAndHandle(route, "localhost", 8000)
+  val server = Http().bindAndHandle(route, "localhost", 8000)
+
+  sys.addShutdownHook(server.map(_.unbind()))
 }
